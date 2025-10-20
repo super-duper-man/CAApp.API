61cff5c45041eaf18c75cf64b022ccb33bf60e9e
Danny <m.r.fouladi7@gmail.com>
2025-10-20 08:17:51 +0330

    References Link and importing DI classes

---
+++ CAApp.API/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using CAApp.Application;
+using CAApp.Infrastructure;
+
+namespace CAApp.API
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddAppDI(this IServiceCollection service)
+        {
+            service.AddApplicationDI()
+                   .AddInfrastructureDI();
+
+            return service;
+        }
+    }
+}
