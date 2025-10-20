2c46934bcbad4fe06d240b1864c6cd448b7eee5e
Danny <m.r.fouladi7@gmail.com>
2025-10-20 08:08:52 +0330

    Add DI Classes

--- CAApp.Application/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CAApp.Application
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ CAApp.Application/DependecyInjection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CAApp.Application
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddApplicationDI(this IServiceCollection service)
+        {
+            return service;
+        }
+    }
+}
